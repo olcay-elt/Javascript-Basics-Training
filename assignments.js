244608d4a82a65284bfb0d3ab63ab70c3a779bd1
olcay-elt <olcay.elt@gmail.com>
2023-01-23 09:45:37 +0100

    Add new style

--- assignments.js
+++ assignments.js
@@ -255,6 +255,36 @@ function checkEmail(email) {
 
 
 
+// * 5)  *****
+
+
+function convertRomaNumber(...args) {
+    const numbers = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    function convert(number) {
+        const arr = []
+        for (const key in numbers) {
+            arr.push(numbers[key])
+        }
+
+
+    }
+
+    for (let i = 0; i < args.length; i++) {
+
+
+    }
+}
+
+
 
 
 
